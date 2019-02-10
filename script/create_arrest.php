9861b4b665aafdd88fc5f20821430667cc0fcf10
DaYmiHn <sawi.denis@yandex.ru>
2019-02-10 18:00:38 +0300

    обновление статей в таблице задержаных

--- script/create_arrest.php
+++ script/create_arrest.php
@@ -1,6 +1,56 @@
 <?php 
 	session_start();
 	include("link_bd.php");
+	$tmp_sql = '';
+	$sql = "SELECT * FROM criminal WHERE fio = '".$_GET['arrested']."'";
+	$tmp_sql = $tmp_sql.$sql."<br>";
+	$result = $connection->query($sql);
+	if ($result->rowCount() == 0) {
+		$sql = "INSERT INTO `criminal` (`id`, `fio`, `article_ykrf`, `article_koaprf`, `hash`) VALUES (NULL,
+		  '".$_GET['arrested']."',
+		 '".$_GET['article_ykrf']."',
+		 '".$_GET['article_koaprf']."',
+		 'хээээш')";
+		 $tmp_sql = $tmp_sql.$sql."<br>";
+		$result = $connection->query($sql);
+	} else {
+		$sql = "SELECT article_koaprf, article_ykrf  FROM criminal WHERE fio = '".$_GET['arrested']."'";
+		$result = $connection->query($sql);
+		$tmp_sql = $tmp_sql.$sql."<br>";
+		$row = $result->fetch();
+		if ($row['article_koaprf'] != '' && $row['article_koaprf']!=$_GET['article_koaprf']){
+			$tmp = $row['article_koaprf']." ".$_GET['article_koaprf'];
+			// $tmp = $_GET['article_ykrf']." ".$row['article_koaprf'];
+			$sql = "UPDATE criminal
+			SET article_koaprf = '".$tmp."' WHERE fio = '".$_GET['arrested']."'";
+			$tmp_sql = $tmp_sql.$sql."<br>";
+			$result = $connection->query($sql);
+		} else {
+			$tmp = $_GET['article_koaprf'];
+			$sql = "UPDATE criminal
+			SET article_koaprf = '".$tmp."' WHERE fio = '".$_GET['arrested']."'";
+			$tmp_sql = $tmp_sql.$sql."<br>";
+			$result = $connection->query($sql);
+		}
+
+
+
+		if ($row['article_ykrf'] != ''){
+			$tmp = $row['article_ykrf']." ".$_GET['article_ykrf'];
+			// $tmp = $_GET['article_ykrf']." ".$row['article_koaprf'];
+			$sql = "UPDATE criminal
+			SET article_ykrf = '".$tmp."' WHERE fio = '".$_GET['arrested']."'";
+			$tmp_sql = $tmp_sql.$sql."<br>";
+			$result = $connection->query($sql);
+		} else {
+			$tmp = $_GET['article_ykrf'];
+			$sql = "UPDATE criminal
+			SET article_ykrf = '".$tmp."' WHERE fio = '".$_GET['arrested']."'";
+			$tmp_sql = $tmp_sql.$sql."<br>";
+			$result = $connection->query($sql);
+		}
+
+	}
 	$sql = "INSERT INTO `arrest` (`id`, `police`, `arrested`, `date`, `article_ykrf`, `article_koaprf`, `region`, `opic`) 
 			VALUES (NULL,
 			 '".$_GET['police']."',
@@ -10,7 +60,7 @@
 			 '".$_GET['article_koaprf']."',
 			 '".$_GET['region']."',
 			 '".$_GET['opic']."')";
-	echo $sql;
+			 $tmp_sql = $tmp_sql.$sql."<br>";
 	$result = $connection->query($sql);
-
+	echo $tmp_sql;
  ?>
